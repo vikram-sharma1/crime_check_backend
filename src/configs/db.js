ae95237c693f39d146a98dcb185508f0a54ccc6a
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-06-28 17:09:16 +0530

    link change

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 
 module.exports = ()=>{
-    return mongoose.connect("mongodb+srv://gourab:gourab2001@cluster0.fgw7e.mongodb.net/?retryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://crime:crime@cluster0.zluli9u.mongodb.net/?retryWrites=true&w=majority")
 }
 
 //"mongodb://localhost:27017/auth-mvc"
